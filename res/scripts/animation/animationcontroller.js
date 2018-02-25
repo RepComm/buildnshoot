bdf40d12cd6df1971a3688fe63b0f32b4f27f829
RepComm <info@jonathancrowder.com>
2018-02-24 21:13:58 -0700

    Make rendering player more compatible with animation code (tree structure, iteratable, sortable, part by name)

--- res/scripts/animation/animationcontroller.js
+++ res/scripts/animation/animationcontroller.js
@@ -1,7 +1,13 @@
 
 class AnimationController {
     constructor () {
+        this.animation = undefined;
+        this.currentClip = 0;
+        this.rig = undefined;
+    }
 
+    setRig (rig) {
+        this.rig = rig;
     }
 }
 
--- res/scripts/entity/player.js
+++ res/scripts/entity/player.js
@@ -1,5 +1,5 @@
 
-const remote = require('electron').remote;
+const AnimationController = require("../animation/animationcontroller.js");
 const Entity = require("./entity.js");
 
 class Player extends Entity {
@@ -9,26 +9,52 @@ class Player extends Entity {
         this.isAlive = false;
         this.mainColor = "#000";
 
-        //Replace states with Animation avatar class
-        this.currentAnimationClip = "walkRight";
-        this.animation = undefined;
+        this.animationController = new AnimationController();
 
-        this.bodyLength = 30;
-        this.headDiameter = 25;
-        this.armHalfLength = 11;
-        this.legHalfLength = 15;
-        this.drawLineWidth = 3;
+        this.body = new LinePart("body", 30);
 
-        this.rightArmRotation = window.radians(45);
-        this.rightForearmRotation = window.radians(10);
-        this.leftArmRotation = window.radians(-45);
-        this.leftForearmRotation = window.radians(10);
+        let head = new CirclePart("head", 25);
+        this.body.appendChild(head);
 
-        this.leftLegRotation = window.radians(15);
-        this.leftForelegRotation = window.radians(15);
+        let rArm = new LinePart("rArm", 11);
+        rArm.position.set(0,7);
+        rArm.rotation = window.radians(45);
+        this.body.appendChild(rArm);
 
-        this.rightLegRotation = window.radians(-15);
-        this.rightForelegRotation = window.radians(15);
+        let rForearm = new LinePart("rForearm", 11);
+        rForearm.position.set(0, 11);
+        rForearm.rotation = window.radians(10);
+        rArm.appendChild(rForearm);
+
+        let lArm = new LinePart("lArm", 11);
+        lArm.position.set(0,7);
+        lArm.rotation = window.radians(-45);
+        this.body.appendChild(lArm);
+
+        let lForearm = new LinePart("lForearm", 11);
+        lForearm.position.set(0, 11);
+        lForearm.rotation = window.radians(10);
+        lArm.appendChild(lForearm);
+
+        let rLeg = new LinePart("rLeg", 11);
+        rLeg.position.set(0, 30);
+        rLeg.rotation = window.radians(-15);
+        this.body.appendChild(rLeg);
+
+        let rForeleg = new LinePart("rForeleg", 11);
+        rForeleg.rotation = window.radians(15);
+        rLeg.appendChild(rForeleg);
+
+        let lLeg = new LinePart("lLeg", 11);
+        lLeg.position.set(0, 30);
+        lLeg.rotation = window.radians(15);
+        this.body.appendChild(lLeg);
+
+        let lForeleg = new LinePart("lForeleg", 11);
+        lForeleg.rotation = window.radians(15);
+        lLeg.appendChild(lForeleg);
+
+        this.animationController.setRig(this.body);
 
         this.position.set(200, 420);
     }
@@ -39,66 +65,98 @@ class Player extends Entity {
         //Move to the player's position
         translate(this.position.x, this.position.y);
 
-        noStroke();
-        fill(this.mainColor);
+        this.body.draw();
 
-        //Draw the head
-        ellipse(0,-this.headDiameter/2,this.headDiameter);
-
-        noFill();
-        strokeWeight(this.drawLineWidth);
-        stroke(this.mainColor);
-
-        //Draw body
-        line(0,0,0,this.bodyLength);
-
-        push(); //====DRAW LEFT ARM
-            translate(0, this.bodyLength/4);
-
-            rotate(this.leftArmRotation);
-            line(0, 0, 0, this.armHalfLength);
-
-            push(); //====DRAW LEFT FOREARM
-                translate(0, this.armHalfLength);
-                rotate(this.leftForearmRotation);
-                line(0,0,0,this.armHalfLength);
-            pop();
-
-            //====DRAW RIGHT ARM
-            rotate( this.rightArmRotation -this.leftArmRotation );
-            line(0, 0, 0, this.armHalfLength);
-
-            //Draw forearm
-            push();
-                translate(0, this.armHalfLength);
-                rotate(this.rightForearmRotation);
-                line(0,0,0,this.armHalfLength);
-            pop();
-        pop(); //====END DRAWING ARMS====
-
-        //Draw legs
-        translate(0, this.bodyLength);
-
-        rotate(this.leftLegRotation);
-        line(0, 0, 0, this.legHalfLength);
-
-        //Draw lower leg
-        push();
-        translate(0, this.legHalfLength);
-        rotate(this.leftForelegRotation);
-        line(0, 0, 0, this.legHalfLength);
         pop();
+    }
+}
 
-        //Continue drawing legs
-        rotate( this.rightLegRotation -this.leftLegRotation );
-        line(0, 0, 0, this.legHalfLength);
+class Part {
+    constructor (name) {
+        this.name = name;
+        this.children = undefined;
+        this.position = window.createVector();
+        this.rotation = 0;
 
-        //Draw lower leg
+        /* Order is instruction on prefered order of parts drawn
+         * Parts will be sorted based on their order, and rendered in that order
+         */
+        this.order = 0;
+    }
+
+    draw () {
+        if (this.children.length) { //A length of 0 is falsy, and the statement won't execute
+            for (let i=0; i<this.children.length; i++) {
+                translate(this.position.x, this.position.y);
+                if (this.children[i] && this.children[i].draw) {
+                    this.children[i].draw();
+                }
+            }
+        }
+    }
+
+    static compare (a, b) {
+        if (a.order === b.order) {
+            return 0;
+        } else {
+            return a.order - b.order;
+        }
+    }
+
+    appendChild (child) {
+        if (!this.children) this.children = [];
+        if (this.children.length === 0) {
+            this.children.push(child);
+        } else {
+            this.children.push(child);
+            this.children.sort(this.compare);
+        }
+    }
+}
+
+class LinePart extends Part {
+    constructor (name, lineLength) {
+        super(name);
+        this.lineLength = lineLength;
+    }
+
+    draw () {
         push();
-        translate(0, this.legHalfLength);
-        rotate(this.rightForelegRotation);
-        line(0, 0, 0, this.legHalfLength);
+        translate(this.position.x, this.position.y);
+
+        line(0,0,0, this.lineLength);
+
+        if (this.children && this.children.length) { //A length of 0 is falsy, and the statement won't execute
+            for (let i=0; i<this.children.length; i++) {
+                if (this.children[i] && this.children[i].draw) {
+                    this.children[i].draw();
+                }
+            }
+        }
+
         pop();
+    }
+}
+
+class CirclePart extends Part {
+    constructor (name, diameter) {
+        super(name);
+        this.diameter = diameter;
+    }
+
+    draw () {
+        push();
+        translate(this.position.x, this.position.y);
+
+        ellipse(0,0, this.diameter, this.diameter);
+
+        if (this.children && this.children.length) { //A length of 0 is falsy, and the statement won't execute
+            for (let i=0; i<this.children.length; i++) {
+                if (this.children[i] && this.children[i].draw) {
+                    this.children[i].draw();
+                }
+            }
+        }
 
         pop();
     }
